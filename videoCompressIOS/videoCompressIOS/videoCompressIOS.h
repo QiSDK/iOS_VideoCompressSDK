17c58a5e6eb6843e3055a544d3606ac09a7eb5b1
subifu3322 <subifu908@gmail.com>
2025-04-15 08:45:41 +0300

    Add files via upload
    初始化

---
+++ videoCompressIOS/videoCompressIOS/videoCompressIOS.h
@@ -0,0 +1,18 @@
+//
+//  videoCompressIOS.h
+//  videoCompressIOS
+//
+//  Created by white on 14/04/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for videoCompressIOS.
+FOUNDATION_EXPORT double videoCompressIOSVersionNumber;
+
+//! Project version string for videoCompressIOS.
+FOUNDATION_EXPORT const unsigned char videoCompressIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <videoCompressIOS/PublicHeader.h>
+
+
